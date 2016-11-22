2e1024d67e5a09a0dffb4e6092d8bfcd8df58e83
Ho Chee Wei <cwho.6018@gmail.com>
2016-11-23 05:48:34 +0800

    Initial commit

---
+++ com.pluralisight.utillib/src/main/java/com/example/util_lib/RunningServiceWrapper.java
@@ -0,0 +1,22 @@
+package com.example.util_lib;
+
+import android.app.ActivityManager;
+
+
+public class RunningServiceWrapper {
+    private ActivityManager.RunningServiceInfo mInfo;
+
+
+    public RunningServiceWrapper(ActivityManager.RunningServiceInfo info) {
+        mInfo = info;
+    }
+
+    public ActivityManager.RunningServiceInfo getInfo() {
+        return mInfo;
+    }
+
+    public String toString() {
+        return mInfo.service.flattenToShortString();
+    }
+}
+
